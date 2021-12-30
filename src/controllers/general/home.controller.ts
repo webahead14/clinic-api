74ae6fd03bef9df487d495d8323d31ae0499ad3f
George Joubran <durd2001@gmail.com>
2021-12-30 12:38:51 +0200

    removed the testing as to not fill up the log

--- src/controllers/general/home.controller.ts
+++ src/controllers/general/home.controller.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils";
-import { fetchQuestionsBySurveyId, fetchMatrixById } from "../../models/survey";
 
 export default catchAsync(async (req: Request, res: Response) => {
-  fetchQuestionsBySurveyId(1);
-  fetchMatrixById(1);
   res.send("Hello world");
 });
