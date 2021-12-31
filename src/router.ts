02b268e7094a916fdc255dcc8ed6732c6508732b
George Joubran <durd2001@gmail.com>
2021-12-31 11:52:35 +0200

    pulling clients from issue #4

--- src/router.ts
+++ src/router.ts
@@ -5,5 +5,5 @@ import generalController from "./controllers/general";
 const router = express.Router();
 
 router.get("/", generalController.home);
-
+router.get("/client/survey/:id", generalController.survey.getSurveyById);
 export default router;
