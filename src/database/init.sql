35a8c961020a3a3536a2b411c823ad6cdd703734
Mario Saliba <mario.saliba98@gmail.com>
2022-01-03 14:37:44 +0200

    fixes the init sql and script building the db

--- src/database/init.sql
+++ src/database/init.sql
@@ -106,7 +106,7 @@ INSERT INTO matrix (id,title,columns,answers,instructions) VALUES(
 INSERT INTO questions (id,question,type,"group",matrix_id,extra_data) VALUES
     (1,'Feeling very upset when something reminds you of the stressful experience?','matrix','group_xyz',1,'{}'),
     (2,'Trouble remembering important parts of the stressful experience?','matrix','group_xyz',1,'{}'),
-    (3,'Loss of interest in activities that you used to enjoy?','matrix','group_xyz',1,'{}'),
+    (3, 'Loss of interest in activities that you used to enjoy?','matrix','group_xyz',1,'{}'),
     (4,'Irritable behaviour, angry outbursts, or acting aggressively?','matrix','group_xyz',1,'{}'),
     (5,'Which choice of the choices below you think it will impact you stress the most?','multiple_choice','group_xyz_multi1',null,'{"multipleChoice":{"choiceType": "Radio","answers": [{"text": "Smoke"},{"text": "Exercise"},{"text": "Drink alcohol"},{"text": "Eat"}]}}'),
     (6,'Mark the type of pains you''ve encountered lately:','multiple_choice','group_xyz_multi2',null,'{"multipleChoice": {"choiceType": "Checkbox","answers": [{"text": "Physical Pain"},{"text": "Mental Pain"},{"text": "Spiritual Pain"}]}}'),
@@ -119,10 +119,10 @@ INSERT INTO clinics VALUES (
 
 INSERT INTO surveys VALUES 
     (1,1,'PCL-5'),
-    (2,1,'PSG-1');
-    (3,1,'ELB-3');
-    (4,1,'SGN-2');
-    (5,1,'LMB-1');
+    (2,1,'PSG-1'),
+    (3,1,'ELB-3'),
+    (4,1,'SGN-2'),
+    (5,1,'LMB-1'),
     (6,1,'RMB-1'
 );
 
