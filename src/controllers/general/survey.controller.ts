318aa9dac096069d734d81e72067a49ff4eca6ea
julio1703 <julio.elias22@gmail.com>
2022-01-13 13:08:29 +0200

    .

--- src/controllers/general/survey.controller.ts
+++ src/controllers/general/survey.controller.ts
@@ -1,21 +1,31 @@
 import { fetchSurveyById } from "../../models/survey";
 import { catchAsync, ApiError } from "../../utils";
 import httpStatus from "http-status";
+import { addAnswers } from "../../models/survey";
 
 const getSurveyById = catchAsync(async (req, res) => {
   let id = req.params.id;
   const data = (await fetchSurveyById(id))[0];
 
-  if (data.is_done)
-    throw new ApiError(httpStatus.BAD_REQUEST, "Survey already done");
+  if (data.is_done) throw new ApiError(httpStatus.BAD_REQUEST, "Survey already done");
 
-  if (data.has_missed)
-    throw new ApiError(httpStatus.BAD_REQUEST, "Survey was missed");
+  if (data.has_missed) throw new ApiError(httpStatus.BAD_REQUEST, "Survey was missed");
 
   res.status(httpStatus.OK).send(data.survey_snapshot);
 });
 
-export default { getSurveyById };
+const insertAnswers = catchAsync(async (req, res) => {
+  const answers = req.body;
+  let query = "";
+  answers.map((element, index) => {
+    query += `(${element.answers},${element.question_id})`;
+    index == answers.length - 1 ? (query += ";") : (query += ",");
+  });
+  addAnswers(query);
+  res.send();
+});
+
+export default { getSurveyById, insertAnswers };
 
 /*
 get from the frontend an array of json which every json contains an answer and its question_id
